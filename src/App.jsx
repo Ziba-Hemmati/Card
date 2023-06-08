76c6633598861b78215612684c4767c7281f7efc
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-06-08 12:19:10 +0330

    fix props

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import Card from "./Components/Card";
 function App() {
   return (
     <div>
-      <Card />
+      <Card
+        image={"../../public/Assets/car.jpeg"}
+        title="Lizard"
+        text="Chevrolet is an iconic American car brand known for its reliable,
+      dependable, and affordable vehicles. Founded in 1911, Chevy has
+      become one of the most recognizable car brands in the world."
+        button={["SHARE", "LEARN MORE"]}
+      />
     </div>
   );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,24 +5,16 @@ import Button from "./Button";
 import "../Styles/card.css";
 import "../Styles/image.css";
 import "../Styles/buttons.css";
-const Card = () => {
+const Card = ({ image, title, text, button }) => {
   return (
     <div className="container">
-      <Image image={"../../public/Assets/car.jpeg"} />
-      <Title title="Lizard" />
-      <Text
-        text={
-          <div>
-            Chevrolet is an iconic American car brand known for its reliable,
-            dependable, and affordable vehicles. Founded in 1911, Chevy has
-            become one of the most recognizable car brands in the world.
-          </div>
-        }
-      />
+      <Image image={image} />
+      <Title title={title} />
+      <Text text={text} />
       <div className="buttons-container">
         <div className="buttons--display">
-          <Button button="SHARE" />
-          <Button button="LEARN MORE" />
+          <Button button={button[0]} />
+          <Button button={button[1]} />
         </div>
       </div>
     </div>
